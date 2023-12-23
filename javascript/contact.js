b54671004fbeb9beeaff52e09ffcae1579c13db7
frankynava22 <frankynava2201@gmail.com>
2023-12-23 03:27:46 -0600

    push

--- javascript/contact.js
+++ javascript/contact.js
@@ -12,4 +12,18 @@ document.addEventListener("DOMContentLoaded", function () {
         // Clear the form fields
         contactForm.reset();
     });
+
+    var toggler = document.getElementById('toggler');
+    toggler.addEventListener('change', function() {
+        var menu = document.querySelector('.menu');
+        if (this.checked) {
+            document.body.style.overflow = 'hidden';
+            menu.style.visibility = 'visible';
+            menu.style.opacity = 1;
+        } else {
+            document.body.style.overflow = 'auto';
+            menu.style.visibility = 'hidden';
+            menu.style.opacity = 0;
+        }
+    });
 });
